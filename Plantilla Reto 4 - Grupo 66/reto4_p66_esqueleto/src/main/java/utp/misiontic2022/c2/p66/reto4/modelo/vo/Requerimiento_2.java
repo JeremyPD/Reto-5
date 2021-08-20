23fcbca8db32863901b886ffcfe2ed4500b1e776
JeremyPD <86785889+JeremyPD@users.noreply.github.com>
2021-08-20 13:45:25 -0500

    Add files via upload

---
+++ Plantilla Reto 4 - Grupo 66/reto4_p66_esqueleto/src/main/java/utp/misiontic2022/c2/p66/reto4/modelo/vo/Requerimiento_2.java
@@ -0,0 +1,49 @@
+package utp.misiontic2022.c2.p66.reto4.modelo.vo;
+
+public class Requerimiento_2 {
+
+    private String proveedor; 
+    private int cantidadTotal;
+ 
+    public Requerimiento_2(String proveedor, int cantidadTotal) {
+        this.proveedor = proveedor; 
+        this.cantidadTotal = cantidadTotal; 
+    }
+ 
+    public String getProveedor() {
+        return proveedor; 
+    }
+ 
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor; 
+    }
+ 
+    public int getCantidadTotal() {
+        return cantidadTotal;
+    }
+ 
+    public void setCantidadTotal(int cantidadTotal) {
+        this.cantidadTotal = cantidadTotal;
+    }
+
+    public String getCantidad() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getNombreMaterial() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getcantidadTotal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getproveedor() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getCantidadtotal() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
